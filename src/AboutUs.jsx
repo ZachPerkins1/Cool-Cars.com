588024c216798d9a6d102c47cbbb9431f857f2e5
Shaan Malhotra <Shaan.Malhotra@cvent.com>
2024-06-06 13:27:26 -0400

    created base content for the page and some  styling

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,14 +1,26 @@
-import { Container, Typography, Card, CardContent, Avatar, Grid } from '@mui/material';
+import { Container, Typography, Card, CardContent, Avatar, Grid, Box } from '@mui/material';
+import { useState } from 'react';
 import './AboutUs.css';
 
 
 
 const AboutUs = () => {
+    const [expandedCard, setExpandedCard] = useState(null);
+
+    const handleCardClick = (personName) => {
+        if (expandedCard === personName) {
+            setExpandedCard(null); // Collapse the card if already expanded
+        } else {
+            setExpandedCard(personName); // Expand the clicked card
+        }
+    };
+
+    const isCardExpanded = (personName) => expandedCard === personName;
     return (
         <Container className="root">
             <Grid container spacing={4} alignItems="center">
                 <Grid item xs={12} md={6}>
-                    <img src="/path/to/your/image.jpg" alt="About Us" className="image" />
+                    <img src="/images/yellow_miata.jpeg" alt=" About Us" className="image" />
                 </Grid>
                 <Grid item xs={12} md={6} className="textContainer">
                     <Typography variant="h3" align="center" gutterBottom>
@@ -20,47 +32,82 @@ const AboutUs = () => {
                 </Grid>
             </Grid>
 
-            <Grid container spacing={4} justifyContent="center">
-                <Grid item xs={12} sm={6} md={4} className="gridItem">
-                    <Card className="card">
-                        <CardContent>
-                            <Avatar className="avatar" alt="Shaan Malhotra" src="/path/to/image.jpg" />
-                            <Typography variant="h5" gutterBottom>
-                                Shaan Malhotra
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Co-Founder
-                            </Typography>
-                        </CardContent>
-                    </Card>
+            <Box className="teamContainer" marginTop={10} border={2} borderColor={'black'}>
+                <Typography variant="h4" align="center" gutterBottom margin={5}>
+                    Meet the Team
+                </Typography>
+                <Grid container spacing={4} justifyContent="center">
+                    <Grid item xs={12} sm={6} md={4} className="gridItem">
+                        <Card className={isCardExpanded('Shaan Malhotra') ? 'expandedCard' : 'card'} onClick={() => handleCardClick('Shaan Malhotra')}>
+                            <CardContent>
+                                <Avatar className="avatar" alt="Shaan Malhotra" src="/images/shaan.png" />
+                                <Typography variant="h5" gutterBottom>
+                                    Shaan Malhotra
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    Co-Founder
+                                </Typography>
+                                {isCardExpanded('Shaan Malhotra') && (
+                                    <>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Email: shaan@example.com
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Phone: +1234567890
+                                        </Typography>
+                                    </>
+                                )}
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} className="gridItem">
+                        <Card className={isCardExpanded('Juan Pinol') ? 'expandedCard' : 'card'} onClick={() => handleCardClick('Juan Pinol')}>
+                            <CardContent>
+                                <Avatar className="avatar" alt="Juan Pinol" src="/images/juan.png" />
+                                <Typography variant="h5" gutterBottom>
+                                    Juan Pinol
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    Co-Founder
+                                </Typography>
+                                {isCardExpanded('Juan Pinol') && (
+                                    <>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Email: juan@example.com
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Phone: +1234567890
+                                        </Typography>
+                                    </>
+                                )}
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} className="gridItem">
+                        <Card className={isCardExpanded('Noah Beito') ? 'expandedCard' : 'card'} onClick={() => handleCardClick('Noah Beito')}>
+                            <CardContent>
+                                <Avatar className="avatar" alt="Noah Beito" src="/images/noah.png" />
+                                <Typography variant="h5" gutterBottom>
+                                    Noah Beito
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    Co-Founder
+                                </Typography>
+                                {isCardExpanded('Noah Beito') && (
+                                    <>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Email: noah@example.com
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Phone: +1234567890
+                                        </Typography>
+                                    </>
+                                )}
+                            </CardContent>
+                        </Card>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6} md={4} className="gridItem">
-                    <Card className="card">
-                        <CardContent>
-                            <Avatar className="avatar" alt="Juan Pinol" src="/path/to/image.jpg" />
-                            <Typography variant="h5" gutterBottom>
-                                Juan Pinol
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Co-Founder
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4} className="gridItem">
-                    <Card className="card">
-                        <CardContent>
-                            <Avatar className="avatar" alt="Noah Beito" src="/path/to/image.jpg" />
-                            <Typography variant="h5" gutterBottom>
-                                Noah Beito
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Co-Founder
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-            </Grid>
+            </Box>
         </Container>
     );
 };
