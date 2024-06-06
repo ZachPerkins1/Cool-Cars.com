db08502f52984f4d87fb4286a87c100e40f20c02
Juan.Pinol <juan.pinol@cvent.com>
2024-06-06 13:58:28 -0400

    added more specific info on card and navbar on inventory page

--- src/InventoryPage.jsx
+++ src/InventoryPage.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import Navbar from './components/NavBar';
 import { Grid, TextField, Checkbox, FormGroup, FormControlLabel } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
@@ -14,39 +15,44 @@ import { red } from '@mui/material/colors';
 function InventoryPage() {
 
     return (
-        <Grid container spacing={4} alignItems="center">
-            <Grid item xs={4}>
-                <Link to={`/`}>
-                    <button variant="contained">Link to Home.</button>
-                </Link>
-                <h2>
-                    Filters:
-                </h2>
-                <FormGroup>
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
-                    <FormControlLabel control={<Checkbox />} label="Required" />
-                    <FormControlLabel control={<Checkbox />} label="Disabled" />
-                </FormGroup>
+        <>
+            <Navbar></Navbar>
+            <Grid container spacing={4} alignItems="center">
+                <Grid item xs={4}>
+                    <Link to={`/`}>
+                        <button variant="contained">Link to Home.</button>
+                    </Link>
+                    <h2>
+                        Filters:
+                    </h2>
+                    <FormGroup>
+                        <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                        <FormControlLabel control={<Checkbox />} label="Required" />
+                        <FormControlLabel control={<Checkbox />} label="Disabled" />
+                    </FormGroup>
+                </Grid>
+                <Grid item xs={8}>
+                    <TextField id="outlined-basic" label="Search for a Car" variant="outlined" />
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="194"
+                            image="https://hips.hearstapps.com/hmg-prod/images/mac01634-1661870031.jpg?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*"
+                            alt="Paella dish"
+                        />
+                        <CardContent>
+                            <Typography variant="body2" color="text.secondary">
+                            Experience the thrill of driving with this stunning 2019 Mazda 
+                            Miata in vibrant Sunshine Yellow. This iconic roadster combines 
+                            classic sports car charm with modern technology, offering an exhilarating 
+                            open-air driving experience.
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
             </Grid>
-            <Grid item xs={8}>
-                <TextField id="outlined-basic" label="Search for a Car" variant="outlined" />
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        height="194"
-                        image="https://mui.com/static/images/cards/paella.jpg"
-                        alt="Paella dish"
-                    />
-                    <CardContent>
-                        <Typography variant="body2" color="text.secondary">
-                            This impressive paella is a perfect party dish and a fun meal to cook
-                            together with your guests. Add 1 cup of frozen peas along with the mussels,
-                            if you like.
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Grid>
-        </Grid>
+        </>
+
     )
 }
 
