6f915b8beaa48185b9e8b8649805d7f2b8cabe5e
Juan.Pinol <juan.pinol@cvent.com>
2024-06-07 16:42:43 -0400

    Finally got the layout working using MUI

--- src/InventoryPage.jsx
+++ src/InventoryPage.jsx
@@ -18,44 +18,46 @@ function InventoryPage() {
     return (
         <>
             <Navbar></Navbar>
-            <Grid container spacing={4} alignItems="center">
-                <Grid item xs={4}>
-                    <Link to={`/`}>
-                        <button variant="contained">Link to Home.</button>
-                    </Link>
+            <Grid container>
+                <Grid item lg={3} sx={{ ml: 12, mt: 4 }}>
                     <h2>
                         Filters:
                     </h2>
                     <FormGroup>
-                        <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
-                        <FormControlLabel control={<Checkbox />} label="Required" />
-                        <FormControlLabel control={<Checkbox />} label="Disabled" />
+                        <FormControlLabel control={<Checkbox />} label="Automatic Transmission" />
+                        <FormControlLabel control={<Checkbox />} label="Manual Transmission" />
+                        <FormControlLabel control={<Checkbox />} label="SUV" />
+                        <FormControlLabel control={<Checkbox />} label="Two Doors" />
+                        <FormControlLabel control={<Checkbox />} label="Four Doors" />
+                        <FormControlLabel control={<Checkbox />} label="Convertible" />
                     </FormGroup>
                 </Grid>
-
-                <Grid container justifyContent="center" columns={1}>
-                    <Grid item >
-                        <TextField id="outlined-basic" label="Search for a Car" variant="outlined" />
-                    </Grid>
-                    <Grid item >
-                    <Grid container columns={2}>
-                    <Grid item >
-                        <CarCard></CarCard>
-                    </Grid>
-                    <Grid item >
-                        <CarCard></CarCard>
-                    </Grid>
-                    <Grid item >
-                        <CarCard></CarCard>
-                    </Grid>
-
-                    </Grid>
-                        <CarCard></CarCard>
+                <Grid lg={8} sx={{ mt: 4 }}>
+                    <h2>Search for a car:</h2>
+                    <TextField id="outlined-basic" label="Search for a Car" variant="outlined" />
+                    <Grid container sx={{mt: 3}} spacing={4}>
+                        <Grid item >
+                            <CarCard></CarCard>
+                        </Grid>
+                        <Grid item >
+                            <CarCard></CarCard>
+                        </Grid>
+                        <Grid item >
+                            <CarCard></CarCard>
+                        </Grid>
+                        <Grid item >
+                            <CarCard></CarCard>
+                        </Grid>
+                        <Grid item >
+                            <CarCard></CarCard>
+                        </Grid>
+                        <Grid item >
+                            <CarCard></CarCard>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
         </>
-
     )
 }
 
