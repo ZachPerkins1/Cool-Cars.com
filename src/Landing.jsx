406597278e3ed9b6ba90f8a1b1de9b1df5501989
noahbeito <noah.beito@cvent.com>
2024-06-10 10:01:09 -0700

    layout and styling updates

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -41,7 +41,7 @@ function LandingPage() {
         <div>
             <Navbar />
             <SalesBanner message={salesBannerMessage} />
-            <Box sx={{ display: 'flex', justifyContent: 'center', mb: 8, mt: 4 }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', mb: 8, mt: 4}}>
                 <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                     <Box sx={{ mt: 6 }}>
                         <Typography variant="h5">Showcasing the coolest cars</Typography>
@@ -52,30 +52,28 @@ function LandingPage() {
                     <img src='images/bogo_sale.png' alt='bogo sale' />
                 </Box>
             </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <Box sx={{ width: '40%', display: 'flex', alignContent: 'center', justifyContent: 'center', ml: 16 }}>
+            <Box sx={{ display: 'flex'}}>
+                <Box sx={{ width: '40%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                     <img src='images/pexels-shkrabaanthony-7144174.jpg' alt='Car dealership' style={{ width: '80%', height: '80%', objectFit: 'cover' }} />
                 </Box>
-                <Box sx={{ width: '60%', display: 'flex', flexDirection: 'column', alignItems: 'center', mr: 16}}>
+                <Box sx={{ width: '60%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', mr: 8 }}>
                     <Button variant="contained" sx={{ width: '50%' }}>Click here to view all inventory</Button>
-                    <ImageList sx={{ width: '100%', height: 450 }} cols={3} rowHeight={200}>
+                    <ImageList sx={{ width: '100%', maxHeight: 450 }} cols={3} rowHeight={200}>
                         {vehicleTypes.map((item) => (
                             <ImageListItem key={item.img} className="vehicle-item" onClick={() => console.log('Clicked')}>
                                 {/* TODO: make images clickable and link to search for that vehicle type */}
-                                <img
-                                    srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                                    src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                                    alt={item.title}
-                                    loading="lazy"
-                                    style={{
-                                        width: '100%',
-                                        height: '100%',
-                                        objectFit: 'contain',
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'center'
-                                    }}
-                                />
+                                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%'}}>
+                                    <img
+                                        srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                        src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                        alt={item.title}
+                                        loading="lazy"
+                                        style={{
+                                            objectFit: 'contain',
+                                            width: '100%',
+                                        }}
+                                    />
+                                </Box>
                                 <div className="vehicle-info">
                                     <Typography variant="h6">{item.title}</Typography>
                                 </div>
