e4a0e275ba91025f5045c548ce6fc705f90c6f24
Shaan Malhotra <Shaan.Malhotra@cvent.com>
2024-06-17 15:53:58 -0400

    finishing touches

--- src/AboutUs/FilterReviews.jsx
+++ src/AboutUs/FilterReviews.jsx
@@ -1,8 +1,8 @@
 import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 const FilterReviews = ({ filter, handleFilterChange }) => (
-    <FormControl fullWidth variant="outlined" style={{ marginBottom: '16px' }}>
-        <InputLabel>Filter Reviews</InputLabel>
+    <FormControl fullWidth variant="outlined" style={{ marginBottom: '16px', backgroundColor: 'white' }}>
+        <InputLabel style={{ color: 'black' }}>Filter Reviews</InputLabel>
         <Select value={filter} onChange={handleFilterChange} label="Filter Reviews">
             <MenuItem value="newest">Newest</MenuItem>
             <MenuItem value="mostPositive">Most Positive</MenuItem>
